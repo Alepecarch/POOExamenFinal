e254fe7d6d155ea10d8e6be42f94742ddf991302
Alepecarch <49079014+Alepecarch@users.noreply.github.com>
2019-05-16 13:16:10 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FinalPregunta4
+namespace FinalPregunta3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string nombre, apellido;
 
+            Console.WriteLine("Porfavor ingrese su Nombre...");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Porfavor ingrese su Apellido...");
+            apellido = Console.ReadLine();
+
+            if (nombre.Length <= 2 || apellido.Length <=2) 
+            {
+                throw new Exception("Nombre/Apellido no válido");
+            }
         }
-    }   
+    }
 }
